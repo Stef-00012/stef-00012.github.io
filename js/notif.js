46a2d4dee5afed7e3dba3a027af073670b51f0c3
Stef-00012 <stefano.delprete.08@gmail.com>
2023-08-20 22:34:20 +0200

    use 4 space indentation instead of 2

--- js/notif.js
+++ js/notif.js
@@ -11,23 +11,23 @@ function Notif(option) {
     el.autoCloseTimeout = (option.autoClose && typeof option.autoCloseTimeout === "number") ? option.autoCloseTimeout : 3000;  
     //Methods
     el.reset = function() {
-      (el.message).innerHTML = "";
-      el.self.classList.remove(el.classNames);
+        (el.message).innerHTML = "";
+        el.self.classList.remove(el.classNames);
     }
     el.showN = function(msg, type) {
-      el.reset();
-      el.message.innerHTML = msg;
-      el.self.style.top = el.top;
-      el.self.classList.add(type);
-      
-      if(el.autoClose) {
-        setTimeout(function() {
-          el.hideN();
-        }, el.autoCloseTimeout);
-      }
+        el.reset();
+        el.message.innerHTML = msg;
+        el.self.style.top = el.top;
+        el.self.classList.add(type);
+        
+        if(el.autoClose) {
+            setTimeout(function() {
+                el.hideN();
+            }, el.autoCloseTimeout);
+        }
     }
     el.hideN = function() {
-      el.self.style.top = '-100%';
-      el.reset();
+    el.self.style.top = '-100%';
+    el.reset();
     };
-  }
+}
