aeeef59092e02913f05505687c7881e3af89ea35
Stef-00012 <stefano.delprete.08@gmail.com>
2024-05-13 20:11:00 +0200

    fix

--- __js/music.js
+++ __js/music.js
@@ -1,59 +1,63 @@
 
 const songList = [
-    '/__assets/music/1-closed_doors.mp3',
-    '/__assets/music/2-talking_to_myself.mp3',
-    '/__assets/music/3-reflection.mp3',
-    '/__assets/music/4-moving_backwards.mp3',
-    '/__assets/music/5-drunk_text.mp3',
-    '/__assets/music/6-broken_by_you.mp3',
-    '/__assets/music/7-be_alright.mp3',
-    '/__assets/music/8-im_nobody_without_you.mp3',
-    '/__assets/music/9-all_the_stars.mp3',
-    '/__assets/music/10-in_the_cards.mp3',
-    '/__assets/music/11-all_these_years.mp3'
+    '/__assets/music/1-closed_doors.mp3',//0
+    '/__assets/music/2-talking_to_myself.mp3',//1
+    '/__assets/music/3-reflection.mp3',//2
+    '/__assets/music/4-moving_backwards.mp3',//3
+    '/__assets/music/5-drunk_text.mp3',//4
+    '/__assets/music/6-broken_by_you.mp3',//5
+    '/__assets/music/7-be_alright.mp3',//6
+    '/__assets/music/8-im_nobody_without_you.mp3',//7
+    '/__assets/music/9-all_the_stars.mp3',//8
+    '/__assets/music/10-in_the_cards.mp3',//9
+    '/__assets/music/11-all_these_years.mp3'//10
 ];
 
 const songNames = [
     {
         name: "Closed Doors",
         artist: "Ismail"
-    },
+    }, //0
     {
         name: "Talking to Myself",
         artist: "Jon Caryl"
-    },
+    },//1
     {
         name: "Reflection",
         artist: "Alexander Stewart"
-    },
+    },//2
+    {
+        name: "Moving Backwards",
+        artist: "Jon Caryl"
+    },//3
     {
         name: "Drunk Text",
         artist: "Henry Moodie"
-    },
+    },//4
     {
         name: "Broken by You",
         artist: "Alexander Stewart"
-    },
+    },//5
     {
         name: "Be Alright",
         artist: "Dean Lewis"
-    },
+    },//6
     {
         name: "I'm Nobody Without You",
         artist: "Jon Caryl"
-    },
+    },//7
     {
-        name: "All The Stas",
+        name: "All The Stars",
         artist: "Hayd"
-    },
+    },//8
     {
         name: "In The Cards",
         artist: "Jamie Miller"
-    },
+    },//9
     {
         name: "All These Years",
         artist: "Alexander Stewart"
-    }
+    }//10
 ]
 
 let currentSongIndex = Math.floor(Math.random() * songList.length);
